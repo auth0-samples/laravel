c7bbbab471c6b245c506b430ee52974849582934
German Lena <german.lena@gmail.com>
2016-09-15 17:41:15 -0300

    removed vendors

--- 00-Starter-Seed/vendor/auth0/login/src/Auth0/Login/Middleware/Auth0OptionalJWTMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Auth0\Login\Middleware;
-
-class Auth0OptionalJWTMiddleware extends Auth0JWTMiddleware {
-
-    protected function validateToken($token) {
-        return true;
-    }
-
-}

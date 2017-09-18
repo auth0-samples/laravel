e01eceb03e46293d5cd7edf38610c191646c47cd
Alexis Luque <aluque@indexlabs.com.uy>
2017-09-18 19:20:40 -0300

    Update Dockerfile

--- 00-Starter-Seed/exec.sh
+++ 00-Starter-Seed/exec.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t auth0-laravel-web-app .
+docker run --rm -it -v $(pwd)/.env:/home/app/.env auth0-laravel-web-app php artisan key:generate
 docker run --env-file .env -p 8000:8000 -it auth0-laravel-web-app

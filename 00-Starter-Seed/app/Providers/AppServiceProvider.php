dda32226e32f88c67124cd08262d97984fca933b
Andres Aguiar <andres.aguiar@auth0.com>
2017-09-22 14:27:48 -0300

    Migrated sample to Laravel 5.5

--- 00-Starter-Seed/app/Providers/AppServiceProvider.php
+++ 00-Starter-Seed/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Cache;
 use Auth0\Login\LaravelCacheWrapper;
+use Auth0\Login\Contract\Auth0UserRepository as Auth0UserRepositoryContract; 
+use Auth0\Login\Repository\Auth0UserRepository as Auth0UserRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,10 +27,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            '\Auth0\Login\Contract\Auth0UserRepository',
-            '\Auth0\Login\Repository\Auth0UserRepository');
-
+        $this->app->bind( 
+            Auth0UserRepositoryContract::class, 
+            Auth0UserRepository::class 
+        ); 
+        
         // $this->app->bind(
         //     '\Auth0\SDK\Helpers\Cache\CacheHandler',
         //     function() {

f2759d9892c0cd849669f84e4001bc40f5ece1d0
Evan Sims <evan.sims@auth0.com>
2020-10-13 22:35:01 -0400

    Move Laravel key generation for Docker/CI into Dockerfile build

--- 01-Login/exec.sh
+++ 01-Login/exec.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
 docker build -t auth0-laravel-01-login .
-docker run --rm -it -v $(pwd)/.env:/home/app/.env auth0-laravel-01-login php artisan key:generate
-docker run --env-file .env -p 3000:3000 -it auth0-laravel-01-login
+
+docker run \
+  --name auth0-laravel-01-login \
+  --rm \
+  --env-file .env \
+  -p 3000:3000 \
+  -it \
+  auth0-laravel-01-login;
